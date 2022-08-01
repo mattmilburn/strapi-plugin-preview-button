12346d1c7102f75c0dcb863031a61ce55d346926
Matt Milburn <me@mattmilburn.com>
2022-08-01 11:17:06 -0500

    Create trimSlashes util

--- server/utils/index.js
+++ server/utils/index.js
@@ -3,9 +3,11 @@
 const getService = require( './get-service' );
 const buildUrl = require( './build-url' );
 const pluginId = require( './plugin-id' );
+const trimSlashes = require( './trim-slashes' );
 
 module.exports = {
   buildUrl,
   getService,
   pluginId,
+  trimSlashes,
 };
