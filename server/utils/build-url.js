226a3ed0bfae39f041551e802e98937950d76165
Matt Milburn <me@mattmilburn.com>
2022-08-01 11:17:23 -0500

    Use trimSlashes in buildUrl util

--- server/utils/build-url.js
+++ server/utils/build-url.js
@@ -2,11 +2,11 @@
 
 const qs = require( 'qs' );
 
-const stripSlashes = str => str ? str.replace( /^\//, '' ).replace( /\/$/, '' ) : '';
+const trimSlashes = require( './trim-slashes' );
 
 const buildUrl = ( baseUrl, basePath, slug, params ) => {
-  const sanitizedBaseUrl = stripSlashes( baseUrl );
-  const sanitizedBasePath = stripSlashes( basePath );
+  const sanitizedBaseUrl = baseUrl ? trimSlashes( baseUrl ) : null;
+  const sanitizedBasePath = basePath ? trimSlashes( basePath ) : null;
   const query = qs.stringify( params, { addQueryPrefix: true } );
   const url = [ sanitizedBaseUrl, sanitizedBasePath, slug ].filter( i => i ).join( '/' );
 
