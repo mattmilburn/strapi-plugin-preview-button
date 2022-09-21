0e61ccb2bbc1c56ba2b2c9c7fd1c39c7811738e5
Matt Milburn <me@mattmilburn.com>
2022-09-21 17:48:59 -0500

    Create usePreviewUrl hook

---
+++ admin/src/hooks/use-preview-url.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react';
+import qs from 'qs';
+
+import { usePluginConfig} from '../hooks';
+import { parseUrl, pluginId } from '../utils';
+
+const usePreviewUrl = ( uid, data, isDraft, isCreating ) => {
+  const [ url, setUrl ] = useState( null );
+  const { config, isLoading } = usePluginConfig();
+  const { contentTypes } = config;
+
+  const match = contentTypes?.find( type => type.uid === uid );
+  const isSupportedType = !! match;
+
+  useEffect( () => {
+    if ( isLoading || isCreating || ! isSupportedType ) {
+      return;
+    }
+
+    const url = parseUrl( match[ isDraft ? 'draft' : 'published' ], data );
+
+    setUrl( url );
+  }, [ isLoading, isCreating ] );
+
+  return {
+    isLoading,
+    isSupportedType,
+    url,
+  };
+};
+
+export default usePreviewUrl;
