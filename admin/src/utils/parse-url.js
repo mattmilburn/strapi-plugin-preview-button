31cb126715c64bac50c8e38e34de3edd5af2d632
Matt Milburn <me@mattmilburn.com>
2022-09-21 18:04:35 -0500

    Fix parseUrl

--- admin/src/utils/parse-url.js
+++ admin/src/utils/parse-url.js
@@ -5,7 +5,17 @@ import trimSlashes from './trim-slashes';
 
 const parseUrl = ( config, data ) => {
   const supportedTypes = [ 'number', 'string' ];
-  const props = Object.entries( data ).map( ( [ _, val ] ) => supportedTypes.includes( typeof val ) );
+  const props = Object.entries( data ).reduce( ( acc, [ key, val ] ) => {
+    if ( ! supportedTypes.includes( typeof val ) ) {
+      return acc;
+    }
+
+    return {
+      ...acc,
+      [ key ]: val,
+    };
+  }, {} );
+
   const url = interpolate( trimSlashes( config.url ), props );
   const query = qs.stringify( config.query, { addQueryPrefix: true } );
 
