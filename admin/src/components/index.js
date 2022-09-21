19ce9d8b7d9916f7fa5d0864f622bd88b7aad52d
Matt Milburn <me@mattmilburn.com>
2022-09-21 14:35:38 -0500

    Create CopyLinkButton component

--- admin/src/components/index.js
+++ admin/src/components/index.js
@@ -1,3 +1,4 @@
+export { default as CopyLinkButton } from './CopyLinkButton';
 export { default as Initializer } from './Initializer';
 export { default as Injector } from './Injector';
 export { default as PreviewButton } from './PreviewButton';
