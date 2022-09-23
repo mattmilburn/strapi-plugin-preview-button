0e441fd36b2efb87afc6f9f07fc210ca21c6cd34
Matt Milburn <me@mattmilburn.com>
2022-09-23 16:43:42 -0500

    Add copy button to ListView

--- admin/src/components/ListViewTableCell/index.js
+++ admin/src/components/ListViewTableCell/index.js
@@ -1,45 +1,72 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useIntl } from 'react-intl';
-import { Button } from '@strapi/design-system';
-import { ExternalLink } from '@strapi/icons';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { Flex, IconButton } from '@strapi/design-system';
+import { stopPropagation, useNotification } from '@strapi/helper-plugin';
+import { ExternalLink, Link } from '@strapi/icons';
 
 import { PREVIEW_WINDOW_NAME } from '../../constants';
 import { getTrad } from '../../utils';
 
-const ListViewTableCell = ( { isDraft, url } ) => {
+const ListViewTableCell = ( { canCopy, isDraft, url } ) => {
   const { formatMessage } = useIntl();
+  const toggleNotification = useNotification();
 
   const handleClick = event => {
     if ( ! url ) {
       return;
     }
 
-    event.preventDefault();
-    event.stopPropagation();
-
     window.open( url, PREVIEW_WINDOW_NAME );
   };
 
   return (
-    <Button
-      size="S"
-      startIcon={ <ExternalLink /> }
-      variant="secondary"
-      onClick={ handleClick }
-    >
-      { formatMessage( isDraft ? {
-        id: getTrad( 'label.open-preview-link' ),
-        defaultMessage: 'Open preview',
-      } : {
-        id: getTrad( 'label.open-link' ),
-        defaultMessage: 'Open link',
-      } ) }
-    </Button>
+    <Flex { ...stopPropagation }>
+      <IconButton
+        onClick={ handleClick }
+        label={ formatMessage( isDraft ? {
+          id: getTrad( 'label.draft' ),
+          defaultMessage: 'Open draft preview',
+        } : {
+          id: getTrad( 'label.published' ),
+          defaultMessage: 'Open live view',
+        } ) }
+        icon={ <ExternalLink /> }
+        noBorder
+      />
+      { canCopy && (
+        <CopyToClipboard
+          text={ url }
+          onCopy={ () => {
+            toggleNotification( {
+              type: 'success',
+              message: {
+                id: 'notification.link-copied',
+                defaultMessage: 'Link copied to the clipboard',
+              },
+            } );
+          } }
+        >
+          <IconButton
+            icon={ <Link /> }
+            label={ formatMessage( isDraft ? {
+              id: getTrad( 'label.copy-preview-link' ),
+              defaultMessage: 'Copy preview link',
+            } : {
+              id: getTrad( 'label.copy-link' ),
+              defaultMessage: 'Copy link',
+            } ) }
+            noBorder
+          />
+        </CopyToClipboard>
+      ) }
+    </Flex>
   );
 };
 
 ListViewTableCell.propTypes = {
+  canCopy: PropTypes.bool,
   isDraft: PropTypes.bool,
   url: PropTypes.string,
 };
