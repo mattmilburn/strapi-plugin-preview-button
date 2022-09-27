55e9e9c9ea8ae04e62d8ca90a7a16564b20ab2c4
Matt Milburn <me@mattmilburn.com>
2022-09-26 20:37:35 -0500

    Use AbortController in usePluginConfig

--- admin/src/hooks/use-plugin-config.js
+++ admin/src/hooks/use-plugin-config.js
@@ -5,31 +5,38 @@ import { request, useNotification } from '@strapi/helper-plugin';
 import { RESOLVE_CONFIG } from '../constants';
 import { pluginId } from '../utils';
 
-const fetchConfig = async ( toggleNotification ) => {
-  try {
-    const endpoint = `/${pluginId}/config`;
-    const data = await request( endpoint, { method: 'GET' } );
-
-    return data ?? {};
-  } catch ( err ) {
-    toggleNotification( {
-      type: 'warning',
-      message: { id: 'notification.error' },
-    } );
-
-    return err;
-  }
-};
-
 const usePluginConfig = () => {
   const dispatch = useDispatch();
   const toggleNotification = useNotification();
   const { config, isLoading } = useSelector( state => state[ `${pluginId}_config` ] );
 
   useEffect( () => {
-    fetchConfig( toggleNotification ).then( data => {
-      dispatch( { type: RESOLVE_CONFIG, data } );
-    } );
+    const abortController = new AbortController();
+
+    const fetchConfig = async () => {
+      try {
+        const endpoint = `/${pluginId}/config`;
+        const data = await request( endpoint, {
+          method: 'GET',
+          signal: abortController.signal,
+        } );
+
+        return data ?? {};
+      } catch ( err ) {
+        if ( ! abortController.signal.aborted ) {
+          toggleNotification( {
+            type: 'warning',
+            message: { id: 'notification.error' },
+          } );
+
+          return err;
+        }
+      }
+    };
+
+    const req = fetchConfig().then( data => dispatch( { type: RESOLVE_CONFIG, data } ) );
+
+    return () => abortController.abort();
   }, [ dispatch, toggleNotification ] );
 
   return { config, isLoading };
