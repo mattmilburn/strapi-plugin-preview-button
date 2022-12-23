f3eafb2189d17b25d0c91cdf36e8e2e726301b43
Matt Milburn <me@mattmilburn.com>
2022-12-22 18:41:43 -0600

    Add custom hook for before build url

--- admin/src/constants.js
+++ admin/src/constants.js
@@ -1,4 +1,6 @@
 import pluginId from './utils/plugin-id';
 
+export const HOOK_BEFORE_BUILD_URL = 'plugin/preview-button/before-build-url';
+
 export const RESOLVE_PREVIEW = `${pluginId}/resolve-data`;
 export const RESOLVE_CONFIG = `${pluginId}/resolve-config`;
