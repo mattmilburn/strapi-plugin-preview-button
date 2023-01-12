6493ea9497120db48424bd25ba3a17454ed857bd
Matt Milburn <me@mattmilburn.com>
2023-01-12 11:59:54 -0600

    Rename Injector component to EditViewRightLinks

--- admin/src/components/index.js
+++ admin/src/components/index.js
@@ -1,5 +1,5 @@
 export { default as CopyLinkButton } from './CopyLinkButton';
+export { default as EditViewRightLinks } from './EditViewRightLinks';
 export { default as Initializer } from './Initializer';
-export { default as Injector } from './Injector';
 export { default as ListViewTableCell } from './ListViewTableCell';
 export { default as PreviewButton } from './PreviewButton';
