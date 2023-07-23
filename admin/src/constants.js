e40ede60a5e5cb166da739c24f24cac56be9db48
Matt Milburn <me@mattmilburn.com>
2023-07-23 09:51:41 -0500

    Fix state management for config data

--- admin/src/constants.js
+++ admin/src/constants.js
@@ -4,3 +4,5 @@ export const ACTION_RESOLVE_PREVIEW = `${pluginId}/resolve-data`;
 export const ACTION_RESOLVE_CONFIG = `${pluginId}/resolve-config`;
 
 export const HOOK_BEFORE_BUILD_URL = 'plugin/preview-button/before-build-url';
+
+export const PREVIEW_WINDOW_NAME = 'StrapiPreview';
--- admin/src/reducers/config.js
+++ admin/src/reducers/config.js
@@ -1,11 +1,13 @@
 import produce from 'immer';
 
-import { ACTION_RESOLVE_CONFIG } from '../constants';
+import { ACTION_RESOLVE_CONFIG, PREVIEW_WINDOW_NAME } from '../constants';
 
 const initialState = {
   isLoading: true,
   config: {
     contentTypes: [],
+    injectListViewColumn: false,
+    openTarget: PREVIEW_WINDOW_NAME,
   },
 };
 
@@ -15,7 +17,7 @@ const configReducer = produce((previousState, action) => {
   switch (action.type) {
     case ACTION_RESOLVE_CONFIG:
       state.isLoading = false;
-      state.config = action.data.config;
+      state.config = action.data;
       break;
 
     default:
