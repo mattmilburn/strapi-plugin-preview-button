f4ded1f47a91333be84e53d03a4c169f127b52c2
Matt Milburn <me@mattmilburn.com>
2023-08-09 18:14:34 -0500

    Catch fetchClient error and use toggleNotification

--- admin/src/hooks/use-plugin-config.js
+++ admin/src/hooks/use-plugin-config.js
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useFetchClient } from '@strapi/helper-plugin';
+import { useFetchClient, useNotification } from '@strapi/helper-plugin';
 
 import { ACTION_RESOLVE_CONFIG } from '../constants';
 import { pluginId } from '../utils';
@@ -8,6 +8,7 @@ import { pluginId } from '../utils';
 const usePluginConfig = () => {
   const dispatch = useDispatch();
   const fetchClient = useFetchClient();
+  const toggleNotification = useNotification();
   const { config, isLoading } = useSelector((state) => state[`${pluginId}_config`]);
 
   useEffect(() => {
@@ -16,13 +17,25 @@ const usePluginConfig = () => {
       return;
     }
 
-    fetchClient.get(`/${pluginId}/config`).then((res) => {
-      dispatch({
-        type: ACTION_RESOLVE_CONFIG,
-        data: res?.data?.config ?? {},
+    fetchClient
+      .get(`/${pluginId}/config`)
+      .then((res) => {
+        dispatch({
+          type: ACTION_RESOLVE_CONFIG,
+          data: res?.data?.config ?? {},
+        });
+      })
+      .catch((err) => {
+        if ('code' in err && err?.code === 'ERR_CANCELED') {
+          return;
+        }
+
+        toggleNotification({
+          type: 'warning',
+          message: { id: 'notification.error' },
+        });
       });
-    });
-  }, [isLoading, config, fetchClient, dispatch]);
+  }, [isLoading, config, dispatch, fetchClient, toggleNotification]);
 
   return {
     data: config,
