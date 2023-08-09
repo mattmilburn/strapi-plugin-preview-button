8758b8bc114b866dafcd8f0ab03d39e24a97c759
Matt Milburn <me@mattmilburn.com>
2023-08-09 17:34:54 -0500

    Replace request with useFetchClient

--- admin/src/hooks/use-plugin-config.js
+++ admin/src/hooks/use-plugin-config.js
@@ -1,52 +1,33 @@
 import { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { request, useNotification } from '@strapi/helper-plugin';
+import { useDispatch, useSelector } from 'react-redux';
+import { useFetchClient } from '@strapi/helper-plugin';
 
 import { ACTION_RESOLVE_CONFIG } from '../constants';
 import { pluginId } from '../utils';
 
 const usePluginConfig = () => {
   const dispatch = useDispatch();
-  const toggleNotification = useNotification();
+  const fetchClient = useFetchClient();
   const { config, isLoading } = useSelector((state) => state[`${pluginId}_config`]);
 
   useEffect(() => {
-    // Do nothing if we have already loaded the config data.
+    // Do nothing if we have already loaded the data.
     if (!isLoading && !!config) {
       return;
     }
 
-    const abortController = new AbortController();
+    fetchClient.get(`/${pluginId}/config`).then((res) => {
+      dispatch({
+        type: ACTION_RESOLVE_CONFIG,
+        data: res?.data?.config ?? {},
+      });
+    });
+  }, [isLoading, config, fetchClient, dispatch]);
 
-    // eslint-disable-next-line consistent-return
-    const fetchData = async () => {
-      try {
-        const endpoint = `/${pluginId}/config`;
-        const data = await request(endpoint, {
-          method: 'GET',
-          signal: abortController.signal,
-        });
-
-        return data ?? {};
-      } catch (err) {
-        if (!abortController.signal.aborted) {
-          toggleNotification({
-            type: 'warning',
-            message: { id: 'notification.error' },
-          });
-
-          return err;
-        }
-      }
-    };
-
-    fetchData().then((data) => dispatch({ type: ACTION_RESOLVE_CONFIG, data }));
-
-    // eslint-disable-next-line consistent-return
-    return () => abortController.abort();
-  }, [isLoading, config, dispatch, toggleNotification]);
-
-  return { config, isLoading };
+  return {
+    data: config,
+    isLoading,
+  };
 };
 
 export default usePluginConfig;
