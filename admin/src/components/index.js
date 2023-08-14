0b96aacadd699c6204dc99a6b2491f4ddfd001d3
Matt Milburn <me@mattmilburn.com>
2023-08-14 16:25:44 -0500

    Remove unnecessary Initializer plugin

--- admin/src/components/index.js
+++ admin/src/components/index.js
@@ -1,5 +1,4 @@
 export { default as CopyLinkButton } from './CopyLinkButton';
 export { default as EditViewRightLinks } from './EditViewRightLinks';
-export { default as Initializer } from './Initializer';
 export { default as ListViewTableCell } from './ListViewTableCell';
 export { default as PreviewButton } from './PreviewButton';
