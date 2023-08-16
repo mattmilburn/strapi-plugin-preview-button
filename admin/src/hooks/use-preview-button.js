e65f4849e80ae564a34e71846651b745cf615630
Matt Milburn <me@mattmilburn.com>
2023-08-16 11:51:16 -0500

    Move openTarget config option again into the uid config

--- admin/src/hooks/use-preview-button.js
+++ admin/src/hooks/use-preview-button.js
@@ -1,24 +1,19 @@
 import { useCallback, useEffect, useMemo, useState } from 'react';
-import get from 'lodash/get';
 import { useStrapiApp } from '@strapi/helper-plugin';
 
 import { HOOK_BEFORE_BUILD_URL, PREVIEW_WINDOW_NAME } from '../constants';
 import usePluginConfig from './use-plugin-config';
-import { parseUrl, pluginId } from '../utils';
+import { parseUrl } from '../utils';
 
 const usePreviewButton = (layout, data, isDraft, isCreating) => {
   const { runHookWaterfall } = useStrapiApp();
   const { data: config, isLoading } = usePluginConfig();
   const [url, setUrl] = useState(null);
+  const [target, setTarget] = useState(PREVIEW_WINDOW_NAME);
   const [canCopy, setCopy] = useState(true);
 
   const { contentType } = layout;
   const { uid } = contentType;
-  const openTarget = get(
-    contentType,
-    ['pluginOptions', pluginId, 'openTarget'],
-    PREVIEW_WINDOW_NAME
-  );
   const uidConfig = config?.contentTypes?.find((type) => type.uid === uid);
   const isSupported = !!uidConfig;
 
@@ -46,8 +41,9 @@ const usePreviewButton = (layout, data, isDraft, isCreating) => {
     }
 
     setUrl(url);
+    setTarget(options?.openTarget ?? PREVIEW_WINDOW_NAME);
     setCopy(options?.copy === false ? false : true);
-  }, [data, optionsFromConfig, setCopy, setUrl, runHookWaterfall]);
+  }, [data, optionsFromConfig, setCopy, setTarget, setUrl, runHookWaterfall]);
 
   useEffect(() => {
     if (!isSupported || isLoading || isCreating) {
@@ -61,7 +57,7 @@ const usePreviewButton = (layout, data, isDraft, isCreating) => {
     canCopy,
     isLoading,
     isSupported,
-    openTarget,
+    openTarget: target,
     url,
   };
 };
