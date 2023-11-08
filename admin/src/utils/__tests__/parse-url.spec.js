aae115e1eb73b5364b17f1417ffce8c17d2e0594
Matt Milburn <me@mattmilburn.com>
2023-11-08 11:03:57 -0600

    Fix encoding method in parseUrl util

--- admin/src/utils/__tests__/parse-url.spec.js
+++ admin/src/utils/__tests__/parse-url.spec.js
@@ -51,7 +51,7 @@ describe('parseUrl', () => {
     expect(result).toEqual(output);
   });
 
-  it('should not double-encode query string params', () => {
+  it('should escape pre-existing % characters when encoding query string params', () => {
     const config = {
       url: 'https://www.example.com/{slug}',
       query: {
@@ -61,9 +61,9 @@ describe('parseUrl', () => {
     };
     const data = {
       slug: 'test',
-      foobar: 'foo%2Fbar%2Ftest',
+      foobar: 'foo%bar%test',
     };
-    const output = 'https://www.example.com/test?type=page&foobar=foo%2Fbar%2Ftest';
+    const output = 'https://www.example.com/test?type=page&foobar=foo%25bar%25test';
     const result = parseUrl(config, data, true);
 
     expect(result).toEqual(output);
