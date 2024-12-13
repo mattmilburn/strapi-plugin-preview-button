ff39b67ec7f94616e30b4185b6e5f01654b8cd43
Matt Milburn <me@mattmilburn.com>
2024-12-13 12:33:21 -0600

    Add type for hook props

--- admin/src/hooks/usePreviewButton.ts
+++ admin/src/hooks/usePreviewButton.ts
@@ -14,6 +14,13 @@ export interface UsePreviewButtonReturn {
   published: PreviewButtonStateConfig | null;
 }
 
+export interface PreviewButtonBeforeBuildUrlProps {
+  data: any;
+  draft: PreviewButtonStateConfig | undefined;
+  published: PreviewButtonStateConfig | undefined;
+  uid: UID.ContentType;
+}
+
 const usePreviewButton = (uid: UID.ContentType | undefined, data: any): UsePreviewButtonReturn => {
   const runHookWaterfall = useStrapiApp(PLUGIN_ID, (value) => value.runHookWaterfall);
   const { data: config, isLoading } = usePluginConfig();
@@ -22,16 +29,20 @@ const usePreviewButton = (uid: UID.ContentType | undefined, data: any): UsePrevi
   const [published, setPublished] = useState<PreviewButtonStateConfig | null>(null);
 
   const uidConfig = config?.contentTypes?.find((type) => type.uid === uid);
-  const isSupported = !!uidConfig;
+  const isSupported = !!uid && !!uidConfig;
 
   const compileWithHooks = useCallback(async () => {
+    if (!isSupported) {
+      return;
+    }
+
     // Run async hook then set state.
     const result = await runHookWaterfall(HOOK_BEFORE_BUILD_URL, {
       data,
-      draft: uidConfig?.draft,
-      published: uidConfig?.published,
+      draft: uidConfig.draft,
+      published: uidConfig.published,
       uid,
-    });
+    } satisfies PreviewButtonBeforeBuildUrlProps);
 
     const draftConfig = getPublishStateConfig(result?.draft as PreviewButtonStateConfig, data);
     const publishedConfig = getPublishStateConfig(
